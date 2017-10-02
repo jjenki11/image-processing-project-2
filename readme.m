dd0caa29c05598acc0b01caeafbdfc7d5e531dbc
jjenki11 <jeff.jenkins.1986@gmail.com>
2017-10-02 18:31:42 -0400

    Added readme

---
+++ readme.m
@@ -0,0 +1,9 @@
+
+
+%   For changing a file and updating repo
+%       git add <file you changed>
+%       git commit -m "<text about what you changed>"
+%       git push
+
+%   For updating repo after someone else has changed
+%       git pull
